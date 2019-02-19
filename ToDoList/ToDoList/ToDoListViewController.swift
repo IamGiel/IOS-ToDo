e3bba5f1bf16e31516cec4b4111cdc84101b5254
IamGiel <november14giel@gmail.com>
2019-02-19 16:38:30 -0500

    solved 'must register nib or a class for the identifier or connect a prototype cell in a storyboard' problem

---
+++ ToDoList/ToDoList/ToDoListViewController.swift
@@ -0,0 +1,36 @@
+//
+//  ViewController.swift
+//  ToDoList
+//
+//  Created by Alpha.giel DeAsis on 2/19/19.
+//  Copyright © 2019 Alpha.giel DeAsis. All rights reserved.
+//
+
+import UIKit
+
+class ToDoListViewController: UITableViewController {
+    
+    let itemArray = ["Draw a design", "Buy groceries", "Sing a song"];
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+    
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return itemArray.count;
+    }
+    
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+//        let cell = tableView.dequeueReusableCell(withIdentifier: "ToDoItemCell", for: indexPath) as UITableViewCell
+        tableView.register(UITableViewCell.self, forCellReuseIdentifier: "ToDoItemCell")
+
+        let cell = tableView.dequeueReusableCell(withIdentifier:"ToDoItemCell", for: indexPath);
+        cell.textLabel?.text = itemArray[indexPath.row]
+        
+        return cell;
+    }
+
+    
+}
+
